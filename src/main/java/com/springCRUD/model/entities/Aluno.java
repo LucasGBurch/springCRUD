86b45ed38f0a3d826d66a9b1d68cd011fde2adee
LucasGBurch <88804181+LucasGBurch@users.noreply.github.com>
2022-02-14 14:32:45 -0300

    Subclasse Aluno e radiobutton
    Criação da subclasse Aluno, que estende Pessoa só que tem nota também. Iniciei a construção do método html/css que faz uso do radioButton/checkbox para marcar se é Aluno ou não, para abrir caixa para digitar a nota caso seja. Falta acertar a mão na classe "reveal-if-active", para depois poder começar a testar o site com banco de dados.

---
+++ src/main/java/com/springCRUD/model/entities/Aluno.java
@@ -0,0 +1,36 @@
+package com.springCRUD.model.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Aluno extends Pessoa {
+
+	@Min(0)
+	@Max(10)
+	private double notaAluno;
+
+	public Aluno() {
+
+	}
+
+	public Aluno(@NotBlank String nome, @NotBlank String telefone,
+			@NotBlank Date nascimento, @NotBlank Date cadastro,
+			@NotBlank Date alteracao, @Min(0) @Max(10) double notaAluno) {
+		super(nome, telefone, nascimento, cadastro, alteracao);
+		this.notaAluno = notaAluno;
+	}
+
+	public double getNotaAluno() {
+		return notaAluno;
+	}
+
+	public void setNotaAluno(double notaAluno) {
+		this.notaAluno = notaAluno;
+	}
+
+}
